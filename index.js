715dffa4e431799240e7abb110ff9820934e4f17
facundovanni <facundovanni@gmail.com>
2018-10-17 02:28:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app'));
-app.use(express.static(__dirname + '/bower_components'));
-app.get('/*', function(req,res) {
+app.use(express.static(__dirname + 'app'));
+app.get('/index.html', function (req, res) {
+
+    res.sendFile(__dirname + 'index.html');
     
-res.sendFile(path.join(__dirname+'/app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
