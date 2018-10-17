5f3c522179feb2cda31950451f46b546b5bfa6d4
facundovanni <facundovanni@gmail.com>
2018-10-17 02:02:07 -0300

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/app'));
-
+app.use(express.static(__dirname + '/bower_components'));
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/app/index.html'));
