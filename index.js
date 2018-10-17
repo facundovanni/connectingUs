a77e4a0e3cfe5cdf65dd7348d026f66cbe50738b
facundovanni <facundovanni@gmail.com>
2018-10-17 02:33:55 -0300

    changes

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'app'));
+app.use(express.static('app'));
 app.get('/index.html', function (req, res) {
 
     res.sendFile(__dirname + 'index.html');
-    
+
 });
 
 // Start the app by listening on the default Heroku port
