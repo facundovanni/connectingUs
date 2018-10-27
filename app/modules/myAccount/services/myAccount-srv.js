3b9f6c001613074a32f339647260d3b046e29a2b
facundovanni <facundovanni@gmail.com>
2018-10-27 02:04:44 -0300

    improve

--- app/modules/myAccount/services/myAccount-srv.js
+++ app/modules/myAccount/services/myAccount-srv.js
@@ -1,9 +1,18 @@
 (function myAccountScope(angular) {
     'use strict';
     angular.module('connectingUsCenter.myAccount')
-        .service('MyAccount', ['ServicesModel','__env', function (ServicesModel, __env) {
+        .service('MyAccount', ['ServicesModel', '__env', function (ServicesModel, __env) {
             var that = this;
-            angular.extend(this, ServicesModel.create(__env.apiUrl + '/api/users/:id', null, {}
+            angular.extend(this, ServicesModel.create(__env.apiUrl + '/api/users', null, {
+
+                get: {
+                    method: 'GET',
+                    url: __env.apiUrl + '/api/users/:id',
+                    params: {
+                        id: '@id'
+                    }
+                }
+            }
             ));
 
 
@@ -24,7 +33,7 @@
                     },
                     Phonenumber: undefined,
                     Phonetype: undefined,
-                    AreaCode:undefined
+                    AreaCode: undefined
                 };
             };
         }]);
