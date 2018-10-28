ee4fb9c6ee029d50c800a42a5ab86498d20bfb8f
Flor <>
2018-10-28 17:44:24 -0300

    My offer - get funcionando.

--- app/modules/myOffer/myOffer-con.js
+++ app/modules/myOffer/myOffer-con.js
@@ -3,7 +3,32 @@
   angular.module('connectingUsCenter.myOffer').controller('MyOfferController', ['$scope', 'MyOffer', '$translate', '$state',
     function MyOfferController($scope, MyOffer, $translate, $state) {
       var ctrl = this;
+      ctrl.isLoading = false;
+
+      ctrl.offer = {};
+
+      ctrl.getService = function getService(idService) {
+        ctrl.isLoading = true;
+        MyOffer.getService({ idService: idService }).$promise
+          .then(ctrl.setOffer)
+          .catch(ctrl.onCatchAccount)
+          .finally(ctrl.onFinallyService);
+      };
+
+      ctrl.setOffer = function setOffer(result) {
+        ctrl.offer = result[0];
+      };
 
 
+      ctrl.onFinallyService = function onFinallyService() {
+        ctrl.isLoading = false;
+      };
+
+      ctrl.init = function init() {
+        //The service id is hardcoded for now.
+        ctrl.getService(5);
+      };
+
+      ctrl.init();
     }]);
 })(angular);
--- app/modules/myOffer/services/myOffer-srv.js
+++ app/modules/myOffer/services/myOffer-srv.js
@@ -2,11 +2,12 @@
     'use strict';
     angular.module('connectingUsCenter.myOffer')
         .service('MyOffer', ['ServicesModel', '__env', function (ServicesModel, __env) {
-            angular.extend(this, ServicesModel.create(__env.apiUrl + '/api/services/:id', null, {
-                get:{
-                    method:'GET',
-                    params:{
-                        id: 1,
+            angular.extend(this, ServicesModel.create(__env.apiUrl + '/api/services/?idService=:idService', null, {
+                getService: {
+                    method: 'GET',
+                    isArray: true,
+                    param: {
+                        idService: '@idService'
                     }
                 }
             }));
