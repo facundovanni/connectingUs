d773ff0a5611dbdc6d208b8f1f4e3a897b905490
Flor <>
2018-11-11 14:29:03 -0300

    Saque el timeout y el inactive

--- app/modules/offers/offers-crud-con.js
+++ app/modules/offers/offers-crud-con.js
@@ -38,9 +38,9 @@
         });
 
         file.upload.then(function (response) {
-          $timeout(function () {
+          // $timeout(function () {
             file.result = response.data;
-          });
+          // });
         }, function (response) {
           if (response.status > 0)
             $scope.errorMsg = response.status + ': ' + response.data;
