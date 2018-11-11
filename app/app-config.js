fe57ecb9bd41badcfb2cd1054d64f3da395b222a
facundovanni <facundovanni@gmail.com>
2018-11-11 15:12:31 -0300

    fix

--- app/app-config.js
+++ app/app-config.js
@@ -10,7 +10,7 @@
     angular.module('connectingUsCenter', [
         'ngRoute', 'ngAnimate', 'ngSanitize',
         'ngTouch', 'ui.router',
-        'ui.bootstrap', 'angularSpinner','angular-toastr',
+        'ui.bootstrap', 'angularSpinner','toastr',
         'connectingUsCenter.services',
         'connectingUsCenter.directives',
         'connectingUsCenter.login',
